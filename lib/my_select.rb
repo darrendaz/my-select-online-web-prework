322974902433594ac6b00a73b26d2633534015b4
Darren L <darren.lou@gmail.com>
2019-02-20 23:26:32 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,6 +2,12 @@ def my_select(collection)
  i = 0
  array = []
  while (i < collection.length) do
+   if collection[i].even?
+     yield collection[i]
+     array << collection[i]
+   else
+     
   i+=1
  end
+ return array
 end
